472cb13b315f177082c0da5117fc4f9f6613fc8e
Hh440 <harsh100045@gmail.com>
2024-12-21 22:28:05 +0530

    first commit

---
+++ src/LockUSDT.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+contract LockUSDT {
+
+    constructor (){
+
+    }
+
+    function deposit () public{
+
+    }
+
+    function withdraw() public{
+        
+    }
+ }
